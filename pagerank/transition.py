a5bf013a5ebf6266dcda61ab4d46d57f3c99588a
Tatuya Kamada <kamada@acm.org>
2017-04-07 11:39:23 +0900

    Add transition method.

--- pagerank/transition.py
+++ pagerank/transition.py
@@ -38,21 +38,54 @@ the transition from a state i to a state j in a finite marcov chain.
 """
 
 import numpy as np
+from collections import defaultdict
+from collections import Counter
+import sys
 
 """
-1. link = caluculate link probability by each page
-2. leap = calculate leap probability by each page
+1. link = 0.9/#number of links
+2. leap = 0.1/n
 3. transition = link + leap
 """
-n = 5
-a = np.zeros((n, n))
+#a = np.zeros((n, n))
 
 def link_probability():
   pass
 
-def leap_probability():
-  pass
+
+def make_graph_matrix(filename):
+  with open(filename, 'r') as g:
+    counter = Counter()
+    leap_matrix = None
+    link_matrix = None
+    n = None
+    for i, line in enumerate(g):
+      if i == 0:
+        n = int(line.strip())
+        leap_matrix = np.full((n, n), 0.1/n)
+        link_matrix = np.zeros((n, n))
+        continue
+      data_line = line.strip()
+      if not data_line:
+        break
+      pair_list = data_line.split('  ')
+      link_dict = defaultdict(int)
+      for pair in pair_list:
+        source, destination = pair.split(' ')
+        counter.update([(int(source), int(destination))])
+    # FIXME: link probability calculation is wrong
+    print(counter)
+    for s, d in counter.elements():
+      link_matrix[s][d] = 0.9 / counter[(s, d)]
+    print(link_matrix)
 
 def transition():
-  pass
+  if len(sys.argv) > 1:
+    filename = sys.argv[1]
+  else:
+    print('USAGE:\n python transition.py <graph_data_file>')
+    exit(-1)
+  g_matrix = make_graph_matrix(filename)
 
+if __name__ == "__main__":
+  transition()
