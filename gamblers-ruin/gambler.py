07e847f3850bbd35fbe851513612329ac8e02e72
Tatuya Kamada <kamada@acm.org>
2017-04-23 16:34:14 +0900

    Initially add gambler.py

---
+++ gamblers-ruin/gambler.py
@@ -0,0 +1,13 @@
+"""
+Gambler's ruin
+
+"""
+
+def gambler(stake, goal, trials):
+  pass
+
+if __name__ == '__main__':
+  if len(sys.algv) != 4:
+    print('USAGE:\n python gambler.py <stake> <goal> <trials>')
+    exit(-1)
+
