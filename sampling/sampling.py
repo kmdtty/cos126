b859ca5f69dcfe7437668389e0a33199b582dcae
Tatuya Kamada <kamada@acm.org>
2017-04-25 09:24:53 +0900

    Initially add sampling.py

---
+++ sampling/sampling.py
@@ -0,0 +1,15 @@
+"""
+Sampling
+
+Creating permutation of length n whose first m elements comprise
+a randome sample.
+"""
+import sys
+
+def sample(m, n):
+  pass
+
+if __name__ == '__main__':
+  if len(sys.argv)  != 3:
+    print('USAGE\npython sample.py <m> <n>')
+  sample(m, n)
