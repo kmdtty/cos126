c2bbd1769c3906915c6b008b72c35d25d68fd314
Tatuya Kamada <kamada@acm.org>
2017-04-26 09:19:19 +0900

    Implement sampling.py

--- sampling/sampling.py
+++ sampling/sampling.py
@@ -1,15 +1,26 @@
 """
 Sampling
+--------
 
-Creating permutation of length n whose first m elements comprise
-a randome sample.
+Sample random m integers in range [0, n-1].
+
 """
 import sys
+import random
 
 def sample(m, n):
-  pass
+  sequence = [x for x in range(n)]
+  for i in range(m):
+    r = random.randint(0, n-1)
+    sequence[i] = sequence[r]
+    sequence[r] = i
+  return sequence[0:m]
 
 if __name__ == '__main__':
   if len(sys.argv)  != 3:
-    print('USAGE\npython sample.py <m> <n>')
-  sample(m, n)
+    print('USAGE\n % python sample.py <m> <n>')
+    print(' where m is required length of random integers, n is the integer range')
+    exit(-1)
+  m = int(sys.argv[1])
+  n = int(sys.argv[2])
+  print(sample(m, n))
