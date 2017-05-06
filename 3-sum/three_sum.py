ec4318a70807dadf4b0cd45e12bccb0586cdf30f
Tatuya Kamada <kmdtty@users.noreply.github.com>
2017-05-06 19:18:23 +0900

    Initially add three_sum.py

---
+++ 3-sum/three_sum.py
@@ -0,0 +1,26 @@
+"""
+Three sum
+---------
+
+INPUT: random n sequence
+
+OUTPUT: Count the triples whose sume is zero in the sequence
+
+"""
+import sys
+
+def print_triples(a):
+  pass
+
+def count_triples(a):
+  pass
+
+def three_sum():
+  print('3sum')
+
+if __name__ == '__main__':
+  if len(sys.argv) != 3:
+    print('USAGE:\n python trhee_sum.py <array of numbers>')
+    exit(-1)
+
+  three_sum()
