1aac963fd8ae0f8a7cf2ce72833153b2a1d750f6
Tatuya Kamada <kmdtty@users.noreply.github.com>
2017-05-06 19:17:32 +0900

    Add sampling_minus as an variation of samping.py

---
+++ sampling/sampling_minus.py
@@ -0,0 +1,27 @@
+"""
+Sampling
+--------
+
+Sample random m integers in range [0, n-1].
+
+"""
+import sys
+import random
+
+def sample(m, n):
+  sequence = [x for x in range(n)]
+  for i in range(m):
+    r = random.randint(0, n-1)
+    sequence[i] = sequence[r]
+    sequence[r] = i
+  return sequence[0:m]
+
+if __name__ == '__main__':
+  if len(sys.argv)  != 3:
+    print('USAGE\n % python sample.py <m> <n>')
+    print(' where m is required length of random integers, n is the integer range')
+    exit(-1)
+  m = int(sys.argv[1])
+  n = int(sys.argv[2])
+  # print(sample(m, n))
+  print("\n".join((str(x - n) for x in sample(m, n*2))))
