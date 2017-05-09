a43b2c1c48e36de3b592f64ac936ad1b58dd212d
Tatuya Kamada <kmdtty@users.noreply.github.com>
2017-05-09 09:26:37 +0900

    Add smallworld.py skelton.

---
+++ small-world/smallworld.py
@@ -0,0 +1,16 @@
+"""
+INPUT: Graph
+
+OUTPUT:
+|v|                       := number of verticies
+|E|                       := number of edges
+avg(Kv_i) = sum(Kv_i)/|v| := average degree
+avg(shortestpath(edges))  := average path length
+CC(g) = avg(lcc(v_i))     := local coefficient of the graph
+      = sum(lcc(v_i))/|v|
+
+lcc(v_i) = 2N_v/Kv_i(Kv_i -1)
+"""
+if __name__ == '__main__':
+  pass
+
