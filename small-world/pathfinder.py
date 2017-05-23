8f1ca7c7c74f2a412702a73f74d1729d72a666d5
Tatuya Kamada <kmdtty@users.noreply.github.com>
2017-05-23 16:57:07 +0900

    Add pathfinder.py skelton.

---
+++ small-world/pathfinder.py
@@ -0,0 +1,23 @@
+"""
+"""
+import sys
+from graph import Graph
+
+class PathFinder:
+  def __init__(self, graph, s):
+    """
+     graph -- Graph
+    """
+    pass
+
+  def distance_to(self, v):
+    pass
+
+  def path_to(self, v):
+    pass
+
+
+if __name__ == '__main__':
+  if len(sys.argv) != 3:
+    print('')
+  PathFinder() 
